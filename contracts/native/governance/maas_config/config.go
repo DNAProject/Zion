8e5dd4cf52f0baecf3c02e1a7b5205cd60a4107d
Krain <ssssu8@qq.com>
2022-03-18 22:16:43 +0800

    add maas config native contract

---
+++ contracts/native/governance/maas_config/config.go
@@ -0,0 +1,115 @@
+/*
+ * Copyright (C) 2021 The Zion Authors
+ * This file is part of The Zion library.
+ *
+ * The Zion is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Zion is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with The Zion.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package maas_config
+
+import (
+	"errors"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/contracts/native"
+	"github.com/ethereum/go-ethereum/contracts/native/contract"
+	. "github.com/ethereum/go-ethereum/contracts/native/go_abi/maas_config_abi"
+	"github.com/ethereum/go-ethereum/contracts/native/utils"
+	"github.com/ethereum/go-ethereum/log"
+)
+
+var (
+	gasTable = map[string]uint64{
+		MethodName:         0,
+		MethodBlockAccount: 30000,
+		MethodIsBlocked:    0,
+	}
+)
+
+func InitMaasConfig() {
+	InitABI()
+	native.Contracts[this] = RegisterMaasConfigContract
+}
+
+func RegisterMaasConfigContract(s *native.NativeContract) {
+	s.Prepare(ABI, gasTable)
+
+	s.Register(MethodName, Name)
+	s.Register(MethodBlockAccount, BlockAccount)
+	s.Register(MethodIsBlocked, IsBlocked)
+}
+
+func Name(s *native.NativeContract) ([]byte, error) {
+	return new(MethodContractNameOutput).Encode()
+}
+
+// block account(add account to blacklist map) or unblock account
+func BlockAccount(s *native.NativeContract) ([]byte, error) {
+	ctx := s.ContractRef().CurrentContext()
+	caller := ctx.Caller
+
+	// check authority
+	if err := contract.ValidateOwner(s, caller); err != nil {
+		log.Trace("blockAccount", "ValidateOwner failed", err)
+		return nil, errors.New("invalid authority")
+	}
+
+	// decode input
+	input := new(MethodBlockAccountInput)
+	if err := input.Decode(ctx.Payload); err != nil {
+		log.Trace("blockAccount", "decode input failed", err)
+		return utils.ByteFailed, errors.New("invalid input")
+	}
+
+	// store blacklist
+	key := getBlacklistKey(input.Addr)
+	if input.DoBlock {
+		set(s, key, utils.BYTE_TRUE)
+	} else {
+		del(s, key)
+	}
+
+	// emit event log
+	if err := emitBlockAccount(s, input.Addr, input.DoBlock); err != nil {
+		log.Trace("propose", "emit event log failed", err)
+		return utils.ByteFailed, errors.New("emitBlockAccount error")
+	}
+
+	log.Debug("BlockAccount", input.Addr, input.DoBlock)
+	return utils.ByteSuccess, nil
+}
+
+func getBlacklistKey(addr common.Address) []byte {
+	return utils.ConcatKey(this, []byte(BLACKLIST), addr.Bytes())
+}
+
+func emitBlockAccount(s *native.NativeContract, addr common.Address, doBlock bool) error {
+	return s.AddNotify(ABI, []string{EventBlockAccount}, addr, doBlock)
+}
+
+// block account(add account to blacklist map) or unblock account
+func IsBlocked(s *native.NativeContract) ([]byte, error) {
+	ctx := s.ContractRef().CurrentContext()
+
+	// decode input
+	input := new(MethodIsBlockedInput)
+	if err := input.Decode(ctx.Payload); err != nil {
+		log.Trace("IsBlocked", "decode input failed", err)
+		return utils.ByteFailed, errors.New("invalid input")
+	}
+
+	// store blacklist
+	key := getBlacklistKey(input.Addr)
+	return get(s, key)
+}
