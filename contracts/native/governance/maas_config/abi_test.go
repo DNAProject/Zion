03782f50a84af4451aff75bf608f7426abc5620a
ZJJ-XYH <zhoujunjie1031@126.com>
2022-04-07 11:53:28 +0800

    unit tests for maas_abi

--- contracts/native/governance/maas_config/abi_test.go
+++ contracts/native/governance/maas_config/abi_test.go
@@ -16,6 +16,14 @@ func TestABIShowJonString(t *testing.T) {
 	}
 }
 
+var testAddresses = []common.Address{
+	common.HexToAddress("0x2D3913c12ACa0E4A2278f829Fb78A682123c0125"),
+	common.HexToAddress("0x2D3913c12ACa0E4A2278f829Fb78A682123c0126"),
+	common.HexToAddress("0x2D3913c12ACa0E4A2278f829Fb78A682123c0127"),
+	common.HexToAddress("0x2D3913c12ACa0E4A2278f829Fb78A682123c0128"),
+	common.HexToAddress("0x2D3913c12ACa0E4A2278f829Fb78A682123c0129"),
+}
+
 func TestABIMethodContractName(t *testing.T) {
 
 	enc, err := utils.PackOutputs(ABI, MethodName, contractName)
@@ -26,7 +34,7 @@ func TestABIMethodContractName(t *testing.T) {
 }
 
 func TestABIMethodChangeOwnerInput(t *testing.T) {
-	expect := &MethodChangeOwnerInput{Addr: common.HexToAddress("0x2D3913c12ACa0E4A2278f829Fb78A682123c0125")}
+	expect := &MethodChangeOwnerInput{Addr: testAddresses[0]}
 	enc, err := expect.Encode()
 	assert.NoError(t, err)
 	methodId := hexutil.Encode(crypto.Keccak256([]byte("changeOwner(address)"))[:4])
@@ -38,10 +46,155 @@ func TestABIMethodChangeOwnerInput(t *testing.T) {
 	assert.Equal(t, expect, got)
 }
 
-//func TestABIMethodChangeOwnerOutput(t *testing.T) {
-//	var cases = []struct {
-//		Result bool
-//	}{
-//
-//	}
-//}
+func TestABIMethodChangeOwnerOutput(t *testing.T) {
+	var cases = []struct {
+		Result bool
+	}{
+		{
+			Result: true,
+		},
+		{
+			Result: false,
+		},
+	}
+
+	for _, testCase := range cases {
+		output := &MethodChangeOwnerOutput{Success: testCase.Result}
+		enc, err := output.Encode()
+		assert.NoError(t, err)
+
+		got := new(MethodChangeOwnerOutput)
+		err = got.Decode(enc)
+		assert.NoError(t, err)
+
+		assert.Equal(t, output, got)
+	}
+}
+
+func TestABIMethodGetOwnerOutput(t *testing.T) {
+	var cases = []struct {
+		Addr common.Address
+	}{
+		{
+			Addr: testAddresses[0],
+		},
+		{
+			Addr: testAddresses[1],
+		},
+	}
+
+	for _, testCase := range cases {
+		output := &MethodGetOwnerOutput{Addr: testCase.Addr}
+		enc, err := output.Encode()
+		assert.NoError(t, err)
+		t.Log(output.Addr)
+		got := new(MethodGetOwnerOutput)
+		err = got.Decode(enc)
+		assert.NoError(t, err)
+
+		assert.Equal(t, output, got)
+	}
+}
+
+func TestMethodBlockAccountInput(t *testing.T) {
+	var cases = []struct{
+		Addr common.Address
+		DoBlock bool
+	} {
+		{
+			Addr: testAddresses[0],
+			DoBlock: false,
+		},
+		{
+			Addr: testAddresses[1],
+			DoBlock: true,
+		},
+	}
+
+	for _, testCase := range cases {
+		output := &MethodBlockAccountInput{Addr: testCase.Addr, DoBlock: testCase.DoBlock}
+		enc, err := output.Encode()
+		assert.NoError(t, err)
+
+		got := new(MethodBlockAccountInput)
+		err = got.Decode(enc)
+		assert.NoError(t, err)
+		assert.Equal(t, got, output)
+	}
+}
+
+func TestMethodBlockAccountOutput(t *testing.T){
+	var cases = []struct {
+		Success bool
+	}{
+		{true},
+		{false},
+	}
+
+	for _, testCase := range cases {
+		output := &MethodBlockAccountOutput{Success: testCase.Success}
+		enc, err := output.Encode()
+		assert.NoError(t, err)
+
+		got := new(MethodBlockAccountOutput)
+		err = got.Decode(enc)
+		assert.NoError(t, err)
+
+		assert.Equal(t, got, output)
+	}
+}
+
+func TestMethodIsBlockedInput(t *testing.T) {
+	var cases = []struct{Addr common.Address} {
+		{testAddresses[1]},
+		{testAddresses[0]},
+	}
+
+	for _, testCase := range cases {
+		output := &MethodIsBlockedInput{testCase.Addr}
+		enc, err := output.Encode()
+		assert.NoError(t, err)
+
+		got := new(MethodIsBlockedInput)
+		err = got.Decode(enc)
+		assert.NoError(t, err)
+		assert.Equal(t, got, output)
+	}
+}
+
+func TestMethodIsBlockedOutput(t *testing.T) {
+	var cases = []struct{Success bool} {
+		{true},
+		{false},
+	}
+
+	for _, testCase := range cases {
+		output := &MethodIsBlockedOutput{testCase.Success}
+		enc, err := output.Encode()
+		assert.NoError(t, err)
+
+		got := new(MethodIsBlockedOutput)
+		err = got.Decode(enc)
+		assert.NoError(t, err)
+		assert.Equal(t, got, output)
+	}
+}
+
+func TestMethodGetBlacklistOutput(t *testing.T) {
+	// ToDo: add some test case results
+	var cases = []struct{Result string} {
+		{""},
+		{""},
+	}
+
+	for _, testCase := range cases {
+		output := &MethodGetBlacklistOutput{testCase.Result}
+		enc, err := output.Encode()
+		assert.NoError(t, err)
+
+		got := new(MethodGetBlacklistOutput)
+		err = got.Decode(enc)
+		assert.NoError(t, err)
+		assert.Equal(t, got, output)
+	}
+}
